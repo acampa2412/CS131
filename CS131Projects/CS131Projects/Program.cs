cbf051c085bc5dccad7fe76ea474df9b2e1c02a6
Anthony Campa <acampa2412@gmail.com>
2021-02-18 21:26:47 -0700

    Module 2

---
+++ CS131Projects/CS131Projects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS131Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NBATeams East = new NBATeams();
+            East.TeamName = "Miami Heat";
+            
+
+            Console.WriteLine("My favorite basketball team is the " + East.TeamName);
+        }
+    }
+}
+
+public class NBATeams
+{
+    public string TeamName { get; set; }
+    
+}
