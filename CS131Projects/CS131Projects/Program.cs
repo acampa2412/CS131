758187170dd1314aa55c85e093748d5e7bdf9b46
Anthony Campa <acampa2412@gmail.com>
2021-03-07 15:18:25 -0700

    Module 6 REDO

--- CS131Projects/CS131Projects/Program.cs
+++ CS131Projects/CS131Projects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CS131Projects.DTOClasses;
 using CS131Projects.MyClasses;
 
@@ -9,10 +10,23 @@ namespace CS131Projects
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] myArray = new string[10] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+            List<string> basketballTeams = new List<string>();
+            basketballTeams.Add("Miami Heat");
+            basketballTeams.Add("Los Angeles Lakers");
+            basketballTeams.Add("Brooklyn Nets");
+            basketballTeams.Add("Orlando Magic");
+            basketballTeams.Add("Dallas Mavericks");
+            basketballTeams.Add("New York Knicks");
+            basketballTeams.Add("Phoenix Suns");
+            basketballTeams.Add("Atlanta Hawks");
+            basketballTeams.Add("Portland Trailblazers");
+            basketballTeams.Add("Boston Celtics");
 
-            foreach (var item in myArray)
-                Console.WriteLine(item);
+
+
+            foreach (var item in basketballTeams)
+                Console.WriteLine(item + ", ");
         }
     }
 }
